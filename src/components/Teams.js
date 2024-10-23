5e088a3760ff1c50b76d7b3173adc6d02b2b1a33
SakshiPatel-20 <141473943+SakshiPatel-20@users.noreply.github.com>
2024-10-23 18:29:47 +0530

    Update Teams.js
    Breakpoints in team slider is updated.

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -20,21 +20,35 @@ function Teams() {
         adaptiveHeight: true,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1280,
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 3,
-                    infinite: true,
                     dots: true,
                 },
             },
-            
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true,
+                },
+            },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
                     rows: 2,
+                },
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    rows: 1,
                 },
             },
         ],
